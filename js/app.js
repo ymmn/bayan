a43e67016cbac43196b0faf1b1e63349e92501b5
ymmn <abdnimery92@gmail.com>
2014-09-02 02:09:08 -0700

    playable!

--- js/app.js
+++ js/app.js
@@ -5,38 +5,38 @@ requirejs.config({
     app: 'app'
   },
   shim: {
-    'lib/teoria' : {
+    'lib/teoria': {
       exports: 'teoria'
     },
-    'lib/subcollider' : {
+    'lib/subcollider': {
       exports: 'sc'
     },
-    'lib/timbre' : {
+    'lib/timbre': {
       exports: 'T'
     },
-    'lib/easeljs' : {
+    'lib/easeljs': {
       exports: 'createjs'
     }
   }
 });
 
 requirejs(['app/bayan'],
-function (Bayan) {
+  function(Bayan) {
 
-  var canvas = document.getElementById('bayan');
-  var context = canvas.getContext('2d');
-  var textArea = document.getElementById('bayan-textarea');
-  var sheetMusicString = 'rsa ecde srgu yhgr bv rsa ecde srgu yhgr bv hybtg ser erv hybtg iii r hybtg ser erv tvr rrr rvgres rrr rvg rrr rvgres ggg rgh grs sxebbe cuuuhbgres grs sxebbe cbbbgvrsai';
-  var bayan = new Bayan(canvas, textArea, sheetMusicString);
+    window.Bayan = Bayan;
+    var canvas = document.getElementById('bayan');
+    var context = canvas.getContext('2d');
+    var textArea = document.getElementById('bayan-textarea');
+    var bayan = new Bayan(canvas, textArea);
 
-  window.addEventListener('resize', onResize, false);
-  onResize();
+    window.addEventListener('resize', onResize, false);
+    onResize();
 
-  function onResize() {
-    w = window.innerWidth;
-    h = window.innerHeight;
-    bayan.resize(w, h);
+    function onResize() {
+      w = window.innerWidth;
+      h = window.innerHeight;
+      bayan.resize(w, h);
+    }
+
   }
-
-
-});
+);
